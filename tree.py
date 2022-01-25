713522b35dacb1f6fd3dee0b1c29318d9e5cb2c1
Aadesh <aadeshnpn@byu.edu>
2022-01-24 21:28:13 -0500

    column wise print tree

--- tree.py
+++ tree.py
@@ -69,6 +69,40 @@ def dfs(root):
     return data
 
 
+def bfsM(root):
+    data = []
+    queue = []
+    column = 5
+    queue.append((root,column))
+    left = False
+    right = False
+    while len(queue) > 0:
+        nd,colno = queue.pop(0)
+        data.append((nd.data, colno))
+        if nd.left is not None:
+            queue.append((nd.left, colno-1))
+        if nd.right is not None:
+            queue.append((nd.right, colno+1))
+    return data
+
+
+def printcolwise(root):
+    data = bfsM(root)
+    coldict = dict()
+    for val,col in data:
+        try:
+            coldict[col] += [val]
+        except KeyError:
+            coldict[col] = [val]
+    maxcol = max(coldict.keys())
+    mincol = min(coldict.keys())
+
+    final = []
+    for i in range(mincol, maxcol):
+        final += coldict[i]
+    print(final)
+
+
 def main():
     root = node(1)
     two = node(2)
@@ -92,15 +126,17 @@ def main():
     four.right = nine
     seven.left = ten
     seven.right = eleven
-    array = []
-    inorderT(root)
-    print()
-    preorderT(root)
-    print()
-    postorderT(root)
-    print()
-    print(bfs(root))
-    print(dfs(root))
+    # array = []
+    # inorderT(root)
+    # print()
+    # preorderT(root)
+    # print()
+    # postorderT(root)
+    # print()
+    # print(bfs(root))
+    # print(dfs(root))
+    # print(bfsM(root))
+    printcolwise(root)
 
 
 if __name__ == '__main__':
