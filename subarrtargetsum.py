114a58f0d583a7fc3544b1826e52349b60fbf143
Aadesh <aadeshnpn@byu.edu>
2022-01-24 22:32:18 -0500

    O(N) time for sub array sub target

--- subarrtargetsum.py
+++ subarrtargetsum.py
@@ -16,14 +16,29 @@ def sumtarget(arr, target):
                 end += 1
             else:
                 break
+    return False
 
+
+def sumtargetdict(arr, target):
+    prefix = {0:0}
+    cursum = 0
+    for j in range(len(arr)):
+        cursum =  cursum+ arr[j]
+        compliment =  cursum - target
+        print(prefix, cursum, compliment)
+        if compliment in prefix:
+            return True, # prefix[compliment], j+1
+        else:
+            prefix[cursum] = j + 1
     return False
 
 
 def main():
     arr = [1,2,5,2,3,10,7]
-    for i in range(1,31):
-        print(i, sumtarget(arr, i))
+    # for i in range(1,31):
+    #     print(i, sumtarget(arr, i), end=' ')
+    #     print(sumtargetdict(arr, i))
+    print(sumtargetdict(arr, 14))
 
 
 if __name__ == '__main__':
